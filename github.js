e415b9e18cd99dfb2509025d3d68af2757ec07ce
Mahendra Choudhary <jakepintu@gmail.com>
2018-07-26 09:15:57 +0530

    Display The Profile

--- github.js
+++ github.js
@@ -1,17 +1,16 @@
 class Github {
-    constructor() {
-        this.client_id = "9834b197f2d5424a31b8";
-        this.client_secret = "2ed508f6e22b03764b4e29d8f232f9379e1cac58";
+  constructor() {
+    this.client_id = 'd9308aacf8b204d361fd';
+    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile
     }
-
-    async getUsers(user) {
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-
-        const profile = await profileResponse.json();
-
-        return {
-            profile
-        }
-    }
+  }
 }
-
